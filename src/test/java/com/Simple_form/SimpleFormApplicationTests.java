38cc338a77fa2c8cd963d3e6d6e37885159bb899
thrishaaaa <thrishakiran14@gmail.com>
2025-07-31 19:50:21 +0530

    first commit

---
+++ src/test/java/com/Simple_form/SimpleFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Simple_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
